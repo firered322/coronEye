c92a03e36293fd7f6481156863eebfdf112ce049
atul singh <atul.singh@myglamm.com>
2020-03-20 19:10:39 +0530

    aded dropdown

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const ejs = require("ejs");
 
 const app = express();
 
-// app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded())
+
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
@@ -16,8 +18,7 @@ app.get("/", (req, res) => {
   res.render("pages/index");
 });
 
-
-app.get("/:country", async (req, res) => {
+app.get("/countries/:country", async (req, res) => {
   const countryName = req.params.country;
   const options = {
     method: "GET",
@@ -38,7 +39,33 @@ app.get("/:country", async (req, res) => {
   }
 });
 
+app.get("/countries", async (req, res) => {
+  const options = {
+    method: "GET",
+    uri: `https://api.covid19api.com/summary`,
+    json: true
+  };
 
+  let response;
+
+  try {
+    response = await rp(options);
+  } catch (error) {
+    console.error(error);
+  }
+
+  let countriesArr = [];
+
+  if (response["Countries"].length) {
+    response["Countries"].forEach(item => {
+      countriesArr.push(item.Country);
+    });
+
+    res.render("pages/countries", {
+      countries: countriesArr
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Running at ${PORT}`);
