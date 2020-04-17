204eeb503f274dba8ad87fcae1f3bf293855966c
Atul <atulsingh1396@gmail.com>
2020-04-17 20:51:36 +0530

    added a new HomePage compoents to house the homepage data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
-import Cards from "./components/Cards/Cards";
-import Chart from "./components/Chart/Chart";
-import CountryPicker from "./components/CountryPicker/CountryPicker";
+// import Cards from "./components/Cards/Cards";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+import HomePage from "./components/utils/HomePage";
 import India from "./components/India/India";
 import Learn from "./components/Learn/Learn";
 import Navbar from "./components/utils/Navbar";
@@ -13,15 +14,15 @@ import Navbar from "./components/utils/Navbar";
 import { fetchData } from "./api";
 
 class App extends React.Component {
-  state = {
-    data: {},
-    country: "",
-  };
+  // state = {
+  //   data: {},
+  //   country: "",
+  // };
 
-  handleCountryChange = async (country) => {
-    const fetchedData = await fetchData(country);
-    this.setState({ data: fetchedData, country: country });
-  };
+  // handleCountryChange = async (country) => {
+  //   const fetchedData = await fetchData(country);
+  //   this.setState({ data: fetchedData, country: country });
+  // };
 
   async componentDidMount() {
     const fetchedData = await fetchData();
@@ -29,33 +30,18 @@ class App extends React.Component {
   }
 
   render() {
-    const { data, country } = this.state;
-    // const homeComponent = () => (
-    //   <div className="container">
-    //     <Cards data={data} />
-    //     <CountryPicker handleCountryChange={this.handleCountryChange} />
-    //     <Chart data={data} country={country} />
-    //   </div>
-    // );
     return (
-      <div>
-        {/* <Navbar /> */}
-        <div className="container">
-          <Cards data={data} />
-          <CountryPicker handleCountryChange={this.handleCountryChange} />
-          <Chart data={data} country={country} />
+
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact component={HomePage} />
+            <Route path="/india" component={India} />
+            <Route path="/learn" component={Learn} />
+          </div>
         </div>
-      </div>
-      // <BrowserRouter>
-      //   <div>
-      //     <Navbar />
-      //     <div className="container">
-      //       <Route path="/" exact component={homeComponent} />
-      //       <Route path="/india" component={India} />
-      //       <Route path="/learn" component={Learn} />
-      //     </div>
-      //   </div>
-      // </BrowserRouter>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/utils/HomePage.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Cards from "../Cards/Cards";
+import Chart from "../Chart/Chart";
+import CountryPicker from "../CountryPicker/CountryPicker";
+
+import { fetchData } from "../../api";
+
+class HomePage extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className="container ">
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
